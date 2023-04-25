d19cbb79459c74d386be22e61c4b83f7fdd39b43
Michael Krumdick <mrkrumdick@gmail.com>
2023-04-25 01:02:48 -0400

    adding activation function to linear layer

--- main.hs
+++ main.hs
@@ -14,32 +14,33 @@ class Layer layer where
 
 data LinearLayer = LinearLayer {
     w :: Parameter,
-    b :: Parameter
+    b :: Parameter,
+    act :: Tensor -> Tensor
 }
 
-mkLinear :: Int -> Int -> IO LinearLayer
-mkLinear nIn nOut = do
+mkLinear :: Int -> Int -> (Tensor -> Tensor) -> IO LinearLayer
+mkLinear nIn nOut actFn = do
         randomW <- randIO' [nIn, nOut]
         w1 <- makeIndependent $ randomW
         randomB <- randIO' [1, nOut]
         b1 <- makeIndependent $ randomB
-        return (LinearLayer w1 b1)
+        return (LinearLayer w1 b1 actFn)
 
 instance Layer LinearLayer where
 
-    numParams LinearLayer{w, b} = 2
+    numParams LinearLayer{w, b, act} = 2
 
-    toList LinearLayer{w, b} = [w, b]
+    toList LinearLayer{w, b, act} = [w, b]
 
-    fromList layer params = do
-            w <- makeIndependent $ (params !! 0)
-            b <- makeIndependent $ (params !! 1)
-            return (LinearLayer w b)
+    fromList LinearLayer{w, b, act} params = do
+            newW <- makeIndependent $ (params !! 0)
+            newB <- makeIndependent $ (params !! 1)
+            return (LinearLayer newW newB act)
     
-    apply LinearLayer{w, b} input = 
+    apply LinearLayer{w, b, act} input = 
             let v1 = (input `matmul` (toDependent w)) 
                 v2 = (toDependent b)
-            in v1 + v2
+            in act (v1 + v2)
 
 type MLP = [LinearLayer]
 
@@ -85,7 +86,7 @@ main = do
 
   let dataList  = (Prelude.take 100 (Prelude.repeat (xi, target)))
   -- initialize model
-  l <- sequence [ (mkLinear 4 3), (mkLinear 3 1) ] 
+  l <- sequence $ [ (mkLinear 4 4 Torch.tanh) | _ <- [1..3]] ++ [(mkLinear 4 1 id)] 
   -- train model
   output <- foldM update l dataList
   -- Evaluate final loss
